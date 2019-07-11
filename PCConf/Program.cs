5968e5ab7dd25cbdffa3998d486bcb21eecc4404
denizzz9998881 <denizzz9998881@gmail.com>
2019-07-11 16:00:37 +0600

    АDDED: HomePC, ProPC

--- PCConf/Program.cs
+++ PCConf/Program.cs
@@ -13,6 +13,27 @@
         public abstract int GetCost();
     }
 
+    class HomePC : PC
+    {
+        public HomePC() : base("Домашняя сборка")
+        { }
+        public override int GetCost()
+        {
+            return 100000;
+        }
+    }
+    class ProPC : PC
+    {
+        public ProPC() : base("Проф. сборка")
+        { }
+        public override int GetCost()
+        {
+            return 450000;
+        }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
