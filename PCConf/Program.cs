70bf4d1926f41530556d5cc1bfc3e56965d02b1b
denizzz9998881 <denizzz9998881@gmail.com>
2019-07-11 15:58:02 +0600

    ADDED: abstract class PC

--- PCConf/Program.cs
+++ PCConf/Program.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PCConf
+﻿namespace PCConf
 {
+
+    abstract class PC
+    {
+        public string Name { get; protected set; }
+
+        public PC(string name)
+        {
+            Name = name;
+        }
+        
+        public abstract int GetCost();
+    }
+
     class Program
     {
         static void Main(string[] args)
