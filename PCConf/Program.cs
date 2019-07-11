06ce38eff73b1ecc13593c1efea0f8a393036137
denizzz9998881 <denizzz9998881@gmail.com>
2019-07-11 16:57:13 +0600

    Main

--- PCConf/Program.cs
+++ PCConf/Program.cs
@@ -227,15 +227,34 @@ namespace PCConf
         static void Main(string[] args)
         {
             PC pc = new CustomPC();
+            HomePC hPC = new HomePC();
+            ProPC pPC = new ProPC();
+            var readyPc = new Facade(hPC, pPC);
             var videoCard = new VideoCard(pc);
             var proc = new Processor(pc);
             var mb = new MotherBoard(pc);
             var chargeBlock = new ChargeBlock(pc);
+            var ownPc = new Facade(pc,videoCard,proc,mb,chargeBlock);
+            // ownPc.BuyOwnPC();
+            Console.WriteLine("Конфигуратор компьютера приветсвует вас!\n1.Домашняя сборка\n2.Pro сборка\n3.Своя сборка");
+            int choose = 0;
+            while (!int.TryParse(Console.ReadLine(), out choose) || choose > 3)
+            {
+                Console.WriteLine("Введите конкретное число!");
+            }
 
-            var facade = new Facade(pc,videoCard,proc,mb,chargeBlock);
-
-            facade.BuyOwnPC();
-            
+            switch(choose)
+            {
+                case 1:
+                    readyPc.BuyHomePC();
+                    break;
+                case 2:
+                    readyPc.BuyProPC();
+                    break;
+                case 3:
+                    ownPc.BuyOwnPC();
+                    break;
+            }
 
             Console.ReadLine();
         }
