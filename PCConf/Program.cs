897110beda03abce3ef3cfff7ecbc6b005a9b042
denizzz9998881 <denizzz9998881@gmail.com>
2019-07-11 16:46:58 +0600

    Added: facade

--- PCConf/Program.cs
+++ PCConf/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace PCConf
-{
+﻿using System;
+using System.Collections.Generic;
 
-    abstract class PC
+namespace PCConf
+{
+    /*            Console.WriteLine("Конфигуратор компьютера приветсвует вас!\n1.Домашняя сборка\n2.Pro сборка\n3.Своя сборка");
+            int choose = 0;
+            while(!int.TryParse(Console.ReadLine(), out choose))
+            {
+                Console.WriteLine("Введите конкретное число!");
+            }*/
+
+    //Фабричный метод
+    // абстрактный класс строительной компании
+    public abstract class PC
     {
         public string Name { get; protected set; }
 
@@ -9,11 +20,12 @@
         {
             Name = name;
         }
-        
+        //фабричный метод
         public abstract int GetCost();
     }
 
-    class HomePC : PC
+    //конкретный пк, домашней сборки
+    public class HomePC : PC
     {
         public HomePC() : base("Домашняя сборка")
         { }
@@ -22,7 +34,9 @@
             return 100000;
         }
     }
-    class ProPC : PC
+
+    //конкретный пк, про сборка
+    public class ProPC : PC
     {
         public ProPC() : base("Проф. сборка")
         { }
@@ -32,7 +46,18 @@
         }
     }
 
-    abstract class PCDecorator : PC
+    public class CustomPC : PC
+    {
+        public CustomPC() : base("Своя сборка")
+        { }
+        public override int GetCost()
+        {
+            return 30000;
+        }
+    }
+
+    //Декоратор
+    public abstract class PCDecorator : PC
     {
         protected PC pc;
 
@@ -42,7 +67,8 @@
         }
     }
 
-    class VideoCard : PCDecorator
+    #region Конкретные декораторы
+    public class VideoCard : PCDecorator
     {
         public VideoCard(PC p)
             : base(p.Name + ", GTX 1050Ti", p)
@@ -54,7 +80,7 @@
         }
     }
 
-    class Processor : PCDecorator
+    public class Processor : PCDecorator
     {
         public Processor(PC p)
             : base(p.Name + ", Intel Core I7 7700K", p)
@@ -66,7 +92,7 @@
         }
     }
 
-    class MotherBoard : PCDecorator
+    public class MotherBoard : PCDecorator
     {
         public MotherBoard(PC p)
             : base(p.Name + ", Asus Monster 200R", p)
@@ -78,7 +104,7 @@
         }
     }
 
-    class ChargeBlock : PCDecorator
+    public class ChargeBlock : PCDecorator
     {
         public ChargeBlock(PC p)
             : base(p.Name + ", AeroCool XPyse", p)
@@ -89,12 +115,117 @@
             return pc.GetCost() + 80000;
         }
     }
+    #endregion
 
+    //Добавлен фасад
+    public class Facade
+    {
+#region Нужные переменные
+        protected PC pc;
+
+        protected HomePC _homePC;
+
+        protected ProPC _proPC;
+
+        List<PCDecorator> decorators = new List<PCDecorator>();
+
+        protected VideoCard _videoCard;
+
+        protected Processor _processor;
+
+        protected MotherBoard _motherBoard;
+
+        protected ChargeBlock _chargeBlock;
+
+        private int _price = 0;
+
+#endregion
+        //конструктор
+        public Facade(PC pc)
+        {
+            this.pc = pc;
+            _price = pc.GetCost();
+        }
+
+        //если в наличии есть стандартные сборки
+        public Facade(HomePC homePC, ProPC proPC)
+        {
+            _homePC = homePC;
+            _proPC = proPC;
+        }
+
+        //инициализация всех компонентов(декораторов)
+        public Facade(VideoCard videoCard, Processor processor, MotherBoard motherBoard, ChargeBlock chargeBlock)
+        {
+            _videoCard = videoCard;
+            _processor = processor;
+            _motherBoard = motherBoard;
+            _chargeBlock = chargeBlock;
+            //добавление каждого декоратора в лист
+            decorators.Add(videoCard);
+            decorators.Add(processor);
+            decorators.Add(motherBoard);
+            decorators.Add(chargeBlock);
+        }
+        //покупка домашней сборки
+        public void BuyHomePC()
+        {
+            Console.WriteLine($"Вы купили компьютер домашней сборки!\n С вас {_homePC.GetCost()}");
+        }
+        //покупка про компа
+        public void BuyProPC()
+        {
+            Console.WriteLine($"Вы купили компьютер проф. сборки!\n С вас {_proPC.GetCost()}");
+        }
+        //сборка своего и покупка пк
+        public void BuyOwnPC()
+        {
+
+            Console.WriteLine("Выберите компоненты: \n");
+            foreach (var decorator in decorators)
+            {
+                int counter = 1;
+                Console.WriteLine($"{counter}.{decorator.Name}| {decorator.GetCost()}");
+                counter++;
+            }
+            Console.WriteLine("5.Buy\n6.Exit");
+
+            int choose = 0;
+            while (choose != 6)
+            {              
+              while (!int.TryParse(Console.ReadLine(), out choose) || choose < 0 || choose > 6)
+                {
+                    Console.WriteLine("Введите корректное число!");
+                }
+
+                switch (choose)
+                {
+                    case 1:
+                        _price += _videoCard.GetCost();
+                        break;
+                    case 2:
+                        _price += _processor.GetCost();
+                        break;
+                    case 3:
+                        _price += _motherBoard.GetCost();
+                        break;
+                    case 4:
+                        _price += _chargeBlock.GetCost();
+                        break;
+                    case 5:
+                        Console.WriteLine($"Стоимость вашего пк: {_price}");
+                        break;
+                }
+            }
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
         }
     }
 }
